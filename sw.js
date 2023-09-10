b2187050d7c76e42cf81f1bb614d961361790836
matper86 <matper86-github@yahoo.com>
2023-09-10 20:36:20 -0300

    primero archivos

--- sw.js
+++ sw.js
@@ -8,7 +8,8 @@ const CACHE_NAME = 'v1_cache_programador',
     "https://kit.fontawesome.com/7027fa2602.js",
     './styles.css',
     './javascript.js',
-    './assets/foto-perfil.png',
+    './assets/favicon.png',
+    './assets/developer.png'
   ]
 self.addEventListener('install', e => {
   e.waitUntil(
